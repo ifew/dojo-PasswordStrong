64de40b136b8c9fb9e63a5bd4b5efa6c21ca0fda
iFew <chitpong@gmail.com>
2018-01-31 00:04:21 +0700

    Added unit test 1234 is false

--- PasswordStrongTest.cs
+++ PasswordStrongTest.cs
@@ -6,9 +6,14 @@ namespace PasswordStrong
     public class PasswordStrongTest
     {
         [Fact]
-        public void Test1()
+        public void Input_1234_Should_Be_Display_False()
         {
+            string input = "1234";
 
+            PasswordStrong passwordStrong = new PasswordStrong();
+            bool actual = passwordStrong.CheckCondition(input);
+
+            Assert.False(actual);
         }
     }
 }
